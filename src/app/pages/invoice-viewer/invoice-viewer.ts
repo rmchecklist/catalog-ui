94bf74fcf0da1c2ee45e4f1d9711950e3f89d741
Rengith Manickam <mrengith@gmail.com>
2025-11-22 22:46:18 -0500

    invoice viewer fix

--- src/app/pages/invoice-viewer/invoice-viewer.ts
+++ src/app/pages/invoice-viewer/invoice-viewer.ts
@@ -70,7 +70,7 @@ export class InvoiceViewerComponent {
     return {
       ...res,
       viewUrl: res.viewUrl || selfView,
-      pdfUrl: res.pdfUrl || null,
+      pdfUrl: res.pdfUrl || undefined,
     };
   }
 }
